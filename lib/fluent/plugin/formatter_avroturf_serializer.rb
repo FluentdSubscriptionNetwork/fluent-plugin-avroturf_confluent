41a84e3d4417921e86273d5964326869fbd7d8bb
kubotat <tkubota@ctc-america.com>
2023-10-24 20:45:48 +0000

    add format_as_json_when_encode_failed option

--- lib/fluent/plugin/formatter_avroturf_serializer.rb
+++ lib/fluent/plugin/formatter_avroturf_serializer.rb
@@ -14,6 +14,7 @@
 # limitations under the License.
 
 require "fluent/plugin/formatter"
+require 'fluent/oj_options'
 require 'avro_turf/messaging'
 require 'avro_turf/confluent_schema_registry'
 
@@ -28,13 +29,15 @@ module Fluent
 
         config_param :namespace, :string, :default => nil, :desc =>  "The namespace of schema"
         config_param :schema_subject, :string, :default => nil, :desc => "The name of subject stored in Schema Registry"
-        config_param :schema_version, :string, :default => "latest", :desc => "The version of subject stored in Schema Registry"
+        config_param :schema_version, :string, :default => nil, :desc => "The version of subject stored in Schema Registry"
 
 	config_param :schemas_path, :string, :default => "./schemas/", :desc => "The directory path where Avro schema files(.avsc) are stored"
 	config_param :schema_name, :string, :default => nil, :desc => "The name of schemas stored in Avro schema files"
 
         config_param :schema_id, :integer, :default => nil, :desc => "The ID of schemas stored in Schema Registry"
-        config_param :validate, :bool, :default => "false", :desc => "Validate the message during encoding process(default: false)"
+        config_param :validate, :bool, :default => false, :desc => "Validate the message during encoding process(default: false)"
+
+        config_param :format_as_json_when_encode_failed, :bool, :default => false, :desc => "Format events as JSON when encoding failed(default: false)"
 
       def configure(conf)
         super
@@ -47,18 +50,27 @@ module Fluent
 	  password: @schema_registry_api_secret,
 	  schemas_path: @schemas_path,
 	)
-
+        if @format_as_json_when_encode_failed
+          @dump_proc = Oj.method(:dump)
+        end
       end
 
       def format(tag, time, record)
-        encoded_data = @avro.encode(
-          record, 
-	  subject: @schema_subject, 
-	  version: @schema_version, 
-	  schema_name: @schema_name, 
-	  schema_id: @schema_id, 
-	  validate: @validate
-	)
+        begin
+          encoded_data = @avro.encode(
+            record, 
+	    subject: @schema_subject, 
+	    version: @schema_version, 
+	    schema_name: @schema_name, 
+	    schema_id: @schema_id, 
+	    validate: @validate
+	  )
+        rescue => e
+          if @format_as_json_when_encode_failed
+            log.debug "Encode failed. Format events as JSON instead.", :error => e.to_s, :error_class => e.class.to_s, :tag => tag
+            "#{@dump_proc.call(record)}#{@newline}"
+          end
+        end
       end
     end
   end
